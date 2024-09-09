331e97aefddd8bed79a93bd66041726bc1ad7fef
arsenid2003@gmail.com <arsenid2003@gmail.com>
2024-09-09 17:57:27 +0500

    Убрал лишние строки и добавил пробелы между операндами

--- console.py
+++ console.py
@@ -1,21 +1,14 @@
 def show_commands():
    print ('exit - закрытие программы\n')
 
-
-
 def run(command_list):
-
-
     print('Adana v0.0: Нейронная сеть, анализирующая изображения на следы редактирования')
     print('https://github.com/AdanaTeam/Final')
     print('Для получения списка команд введите "help"\n')
     finished = False
-    while (not finished):
-    
-        command=input('Введите команду: ')
+    while (not finished):  
+        command = input('Введите команду: ')
         if command in command_list.keys():
             command_list[command]()
         else:
             print(f"Команда  \"{command}\" не найдена.\n")
-
-# run(command_list)
